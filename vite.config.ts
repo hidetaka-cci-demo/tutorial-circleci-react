f453d84d332cb5587fcc405064558cc7ef756cb8
Hidetaka Okamoto CCI <hidetaka.okamoto@circleci.com>
2026-02-12 11:12:25 +0900

    update test config

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 })
